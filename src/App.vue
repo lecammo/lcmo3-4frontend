f9a5c290fc7e58433990042e39a5abba6c1d8024
lecammo <113898641+lecammo@users.noreply.github.com>
2022-10-26 08:06:22 +0700

    khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+
+}
+
+</script>
+
+<template>
+  <header>
+   <h1>Hello, Vue.js!</h1>
+    
+  </header>
+</template>
+
+<style >
+.page{
+  max-width: 400px;
+  margin:auto;
+}
+</style>
